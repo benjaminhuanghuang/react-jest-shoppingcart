4511ad1dd99974415049ca7d2c720e78acc7fe2e
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-09-10 17:35:54 -0700

    refactor

---
+++ app.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const express = require("express");
+
+const app = new express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "www")));
+
+var items = require("./items.js");
+
+var conversions = {
+  USDtoCAD: 1.5,
+  USDtoGBP: 0.9
+};
+
+var locale = {
+  country: "USA"
+};
+
+// use middleware simulate latency
+// server.use(function(req,res,next)
+// {
+//   setTimeout(next,3000)
+// });
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.get("/rates", function(req, res) {
+  res.json(conversions);
+});
+app.get("/items", function(req, res) {
+  res.json(items);
+});
+app.delete("/items/:id", function(req, res) {
+  items = items.filter(item => req.params.id !== item.id);
+});
+app.get("/locale", function(req, res) {
+  res.json(locale);
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,47 +1,10 @@
-const path = require('path');
-const express = require('express');
-const React = require('react');
+const app = require("./app");
 
-const server = new express();
+if (process.env.NODE_ENV === "production") {
 
-server.set("view engine", "ejs");
-server.set("views",  path.join(__dirname, 'views'));
-server.use(express.static(path.join(__dirname, 'www')));
-
-var items = require('./items.js');
-
-var conversions = {
-    USDtoCAD: 1.5,
-    USDtoGBP: 0.9
-};
-
-
-var locale = {
-    country: "USA"
 }
 
-
-// simulate latency
-//server.use(function(req,res,next){setTimeout(next,3000)});
-
-
-server.get('/', function (req, res) {
-        res.render('index');
-    })
-    .get("/rates", function (req, res) {
-        res.json(conversions);
-    })
-    .get("/items", function (req, res) {
-        res.json(items);
-    })
-    .delete("/items/:id", function (req, res) {
-        items = items.filter(item => req.params.id !== item.id);
-    })
-    .get("/locale", function (req, res) {
-        res.json(locale);
-    });
-
-let instance = server.listen(9527, () => {
-    console.info("Express listening on port 9527.");
+const PORT = process.env.PORT || 8010; // for prod deployment
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
 });
-module.exports = instance;
