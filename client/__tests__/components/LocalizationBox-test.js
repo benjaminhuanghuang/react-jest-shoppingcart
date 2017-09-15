4b04212a4ad655898a45b8f1e83899d785342c75
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-09-15 09:27:48 -0700

    update

--- client/__tests__/components/LocalizationBox-test.js
+++ client/__tests__/components/LocalizationBox-test.js
@@ -1,20 +1,26 @@
-"use strict";
 jest.dontMock('../../components/LocalizationBox.js');
 
 import React from 'react';
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 import TestUtils from 'react-dom/test-utils';
+//
+import LocalizationBox from '../../components/LocalizationBox.js';
 
-xdescribe("the localization box",()=>{
+describe("the localization box",()=>{
+	const mockStore = configureStore();
+	const store = mockStore({});
+	
 	it("should display the country associated with the current value",()=>{
-
-		let LocalizationBox = require('../../../app/components/LocalizationBox.js');
 
 		let localeInfo = {
 			country:"GB"
 		};
 
 		let localizationBox = TestUtils.renderIntoDocument(
+			<Provider store={store}>
 			<LocalizationBox country={localeInfo.country} />
+			</Provider>
 		);
 
 		// todo - finalize localization helper
